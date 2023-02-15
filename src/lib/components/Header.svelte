9f24edfc4329a8801dbd376970e571e883f09a4c
Sean Kim <thesean.kim33@gmail.com>
2023-02-14 22:21:57 -0600

    Add credits to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,7 +4,17 @@
 </script>
 
 <div class="navbar top-0 z-50 fixed">
-	<div class="navbar-start"><Logo /></div>
+	<div class="navbar-start">
+		<Logo />
+		<span class="font-serif"
+			>made by <a href="https://sean.kim" class="link bg-orange-200">sean.kim</a>, original design
+			by
+			<a
+				class="link bg-orange-200"
+				href="https://dribbble.com/shots/6919869-Model-Agency-Website-Concept">Anastasia K.</a
+			></span
+		>
+	</div>
 	<div class="navbar-end">
 		<Menu />
 	</div>
