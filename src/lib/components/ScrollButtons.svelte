b13eee854f5b775ea14706ca2ee924b5c2f93d22
Sean Kim <thesean.kim33@gmail.com>
2023-02-18 16:33:43 -0600

    Fix scroll buttons not working in prod

--- src/lib/components/ScrollButtons.svelte
+++ src/lib/components/ScrollButtons.svelte
@@ -7,7 +7,9 @@
 
 	let initialPictureState = {
 		src1: Placeholder,
-		src2: Model2
+		src1Id: 'placeholder',
+		src2: Model2,
+		src2Id: 'model2'
 	};
 	$: pictureState = currentSection(yCoord) || initialPictureState;
 
@@ -17,40 +19,49 @@
 		if (y > 0 && y < 1000) {
 			return {
 				src1: Placeholder,
-				src2: Model2
+				src1Id: 'placeholder',
+				src2: Model2,
+				src2Id: 'model2'
 			};
 		} else if (y > 1000 && y < 2000) {
 			return {
 				src1: Model1,
-				src2: Model3
+				src1Id: 'model1',
+				src2: Model3,
+				src2Id: 'model3'
 			};
 		} else if (y > 2000 && y < 3000) {
 			return {
 				src1: Model2,
-				src2: Model4
+				src1Id: 'model2',
+				src2: Model4,
+				src2Id: 'model4'
 			};
 		} else if (y > 3000) {
 			return {
 				src1: Model3,
-				src2: Placeholder
+				src1Id: 'model3',
+				src2: Placeholder,
+				src2Id: 'placeholder'
 			};
 		}
 	};
 
-	const jumpToSection = (section: string) => {
-		console.log(section);
-		switch (section) {
-			case '/src/lib/assets/model1.png':
+	const jumpToSection = (id: string) => {
+		switch (id) {
+			case 'model1':
 				window.scrollTo({ top: 0, behavior: 'smooth' });
 				break;
-			case '/src/lib/assets/model2.jpeg':
+			case 'model2':
 				window.scrollTo({ top: 1001, behavior: 'smooth' });
 				break;
-			case '/src/lib/assets/model3.png':
+			case 'model3':
 				window.scrollTo({ top: 2001, behavior: 'smooth' });
 				break;
-			case '/src/lib/assets/model4.png':
+			case 'model4':
 				window.scrollTo({ top: 3001, behavior: 'smooth' });
+				break;
+			case 'placeholder':
 				break;
 		}
 	};
@@ -61,12 +72,12 @@
 	<div class="grid grid-cols-2 gap-8 h-48">
 		<div>
 			<p class="font-serif mb-5">Prev.</p>
-			<button on:click={() => jumpToSection(pictureState.src1)}>
+			<button on:click={() => jumpToSection(pictureState.src1Id)}>
 				<img alt="PrevPerson" src={pictureState.src1} class="h-48" />
 			</button>
 		</div>
 		<div>
-			<button on:click={() => jumpToSection(pictureState.src2)}>
+			<button on:click={() => jumpToSection(pictureState.src2Id)}>
 				<img alt="NextPerson" src={pictureState.src2} class="h-48" />
 			</button>
 		</div>
