ebe958626f02fe4922898de145785b38476f0fc5
Sean Kim <thesean.kim33@gmail.com>
2023-02-18 20:42:48 -0600

    Add responsiveness to model sections

--- src/lib/components/ModelSection.svelte
+++ src/lib/components/ModelSection.svelte
@@ -6,32 +6,35 @@
 
 <div class={'w-full h-[100vh] font-serif ' + bioInfo.gradient}>
 	<div class="grid grid-cols-2 grid-rows-6">
-		<div class="flex flex-col h-[100vh] mx-32 items-start justify-center">
+		<div class="flex flex-col h-[100vh] mx-5 md:mx-10 lg:mx-32 items-start justify-center">
 			<div class="py-16 relative">
-				<h1 class={'text-7xl z-10 tracking-wide font-semibold relative ' + bioInfo.nameColor}>
+				<h1
+					class={'text-5xl md:text-7xl z-10 tracking-wide font-semibold relative ' +
+						bioInfo.nameColor}
+				>
 					{bioInfo.name}
 				</h1>
 				<span class={'text-[16rem] absolute -top-28 font-sans font-bold ' + bioInfo.bgColor}
 					>{bioInfo.number}</span
 				>
 			</div>
-			<div class="z-10 grid grid-cols-2 grid-rows-2 gap-28 h-32">
+			<div class="z-10 grid grid-cols-2 grid-rows-2 gap-6 md:gap-28 h-32">
 				<div>
-					<h2 class="text-lg">Height</h2>
-					<p class="text-lg font-bold font-sans tracking-wider pb-3">{bioInfo.height}</p>
+					<h2 class="text-sm md:text-lg">Height</h2>
+					<p class="text-sm md:text-lg font-bold font-sans tracking-wider pb-3">{bioInfo.height}</p>
 
-					<h2 class="text-lg">Eye Colour</h2>
-					<p class="text-lg font-bold font-sans tracking-wider">{bioInfo.eyeColor}</p>
+					<h2 class="text-sm md:text-lg">Eye Colour</h2>
+					<p class="text-sm md:text-lg font-bold font-sans tracking-wider">{bioInfo.eyeColor}</p>
 				</div>
 				<div>
-					<h2 class="text-lg">Weight</h2>
-					<p class="text-lg font-bold font-sans tracking-wider pb-3">{bioInfo.weight}</p>
+					<h2 class="text-sm md:text-lg">Weight</h2>
+					<p class="text-sm md:text-lg font-bold font-sans tracking-wider pb-3">{bioInfo.weight}</p>
 
-					<h2 class="text-lg">Hair Colour</h2>
-					<p class="text-lg font-bold font-sans tracking-wider">{bioInfo.hairColor}</p>
+					<h2 class="text-sm md:text-lg">Hair Colour</h2>
+					<p class="text-sm md:text-lg font-bold font-sans tracking-wider">{bioInfo.hairColor}</p>
 				</div>
 			</div>
-			<div class="my-3 font-sans tracking-wide text-gray-600">
+			<div class="my-3 md:my-6 font-sans tracking-wide text-gray-600 text-xs md:text-base">
 				<p>
 					{bioInfo.bio}
 				</p>
@@ -43,7 +46,11 @@
 			</div>
 		</div>
 		<div class="flex flex-col h-[100vh] items-start justify-center relative">
-			<img alt="ModelOne" src={bioInfo.picture} class="h-3/4 absolute -left-14" />
+			<img
+				alt="ModelOne"
+				src={bioInfo.picture}
+				class="sm:h-3/4 md:h-[50%] lg:h-2/3 xl:h-5/6  md:w-auto absolute -left-3 md:-left-5 lg:-left-8"
+			/>
 		</div>
 	</div>
 </div>
--- src/lib/components/VerticalNav.svelte
+++ src/lib/components/VerticalNav.svelte
@@ -36,7 +36,9 @@
 </script>
 
 <svelte:window bind:scrollY={yCoord} />
-<div class="flex flex-col h-[100vh] justify-center items-center sticky top-0 gap-4 ml-2 sm:ml-0">
+<div
+	class="flex flex-col h-[100vh] justify-center items-center sticky top-0 gap-4 ml-2 sm:ml-0 md:ml-1"
+>
 	<NavDot bind:onPage={NavState[0]} on:click={() => jumpToSection(0)} />
 	<NavDot bind:onPage={NavState[1]} on:click={() => jumpToSection(1)} />
 	<NavDot bind:onPage={NavState[2]} on:click={() => jumpToSection(2)} />
